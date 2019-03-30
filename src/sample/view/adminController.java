55e0b73f4bbb5307bc841332a723e3ef34dce02b
Michele Illiano <mikeilliano3@gmail.com>
2019-03-30 18:50:34 -0400

    Implemented the admin page, admin can now add and delete users

--- src/sample/view/adminController.java
+++ src/sample/view/adminController.java
@@ -11,9 +11,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 import javafx.collections.FXCollections;
 import sample.users.Default;
@@ -37,22 +39,17 @@ public class adminController implements Serializable {
 	private ObservableList<String> obsList;
 
 	//Start method loads up all users to the list view
-	public void start(Stage stage) {   
-		mainStage = stage;
+	public void initialize() {   
 
+		//Get current path of the user directory
+		String dir = System.getProperty("user.dir");
+        String path = dir+"/src/sample/users/";
+        File currentDir = new File(path);
 		//create an ObservableList from an ArrayList
-		ArrayList<String> listOfUsers = new ArrayList<>();
-		/*try {
-			//Get user names from serialized file
-			listOfUsers = fileCreator.songNames();
-		} catch(IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
+		ArrayList<String> listOfUsers = getUserNames(currentDir);
 		//Fill the observable list with the user names gathered from the file
 		obsList =  FXCollections.observableArrayList(listOfUsers);
 		listView.setItems(obsList);
-
 	}
 	
 	@FXML
@@ -63,13 +60,13 @@ public class adminController implements Serializable {
 		Scene scene = new Scene(loginView);
 		Stage window = (Stage) ((Node)evt.getSource()).getScene().getWindow();
 		window.setScene(scene);
-		window.show();
-		
+		window.show();	
 	}
 
 	@FXML
 	public void addUser(ActionEvent evt) throws IOException {
 
+		//TODO add check to make sure that user is unique
 		//Pop up dialog to get user name for the user object
 		TextInputDialog dialog = new TextInputDialog();
 		dialog.setTitle("Add new user");
@@ -77,60 +74,143 @@ public class adminController implements Serializable {
 		dialog.setContentText("Name:");
 		Optional<String> result = dialog.showAndWait();
 
-		//Create the folder for this user in the user folder !!!! Also check to see if user is unquie, do that later!!!!
+		//Create the path for which the users folder will reside
         String dir = System.getProperty("user.dir");
         String PATH = dir+"/src/sample/users/";
 		String directoryName = PATH.concat(result.get());
 
-		System.out.println(directoryName);
-
-		//TODO check to see if user name that admin inputed is unquie!
+		//TODO check to see if user name that admin inputed is unique!
 		File directory = new File(directoryName);
-        if(!directory.canWrite()) { // check if user have write permissions
+		// check if user have write permissions
+        if(!directory.canWrite()) {
+        	// check if the directory already exist
             if (! directory.exists()){
                 boolean successful = directory.mkdirs();
                 if (successful){
                     System.out.println("directory was created successfully");
+                    try{
+            			//Set up the path for the .ser file
+            			directoryName = directoryName.concat("/"+result.get()+".ser");
+            			String filename = directoryName;
+            			Default user = new Default(result.get());
+            			//Saving of object in the .ser file
+            			FileOutputStream file = new FileOutputStream(filename);
+            			ObjectOutputStream out = new ObjectOutputStream(file);
+
+            			// Method for serialization of object
+            			out.writeObject(user);
+            			out.close();
+            			file.close();
+            			System.out.println("Object has been serialized");
+            			//Update the listView
+            			updateListView();
+            		}
+            		catch (IOException e){
+            			System.out.println("IOException is caught");
+            			e.printStackTrace();
+            			System.exit(-1);
+            		}
                 }else{
                     System.out.println("directory was not created");
                 }
-                // If you require it to make the entire directory path including parents,
-                // use directory.mkdirs(); here instead.
             }
         }else{
             System.out.println("PERMISSION DENIED");
         }
-
-		//Folder is created, create user object and serialize it and save it in the directory of that user
-		try{
-			//Set up the path for the .ser file
-			directoryName = directoryName.concat("/"+result.get()+".ser");
-			String filename = directoryName;
-			Default user = new Default(result.get());
-			//Saving of object in a file
-			FileOutputStream file = new FileOutputStream(filename);
-			ObjectOutputStream out = new ObjectOutputStream(file);
-
-			// Method for serialization of object
-			out.writeObject(user);
-			out.close();
-			file.close();
-			System.out.println("Object has been serialized");
-		}
-		catch (IOException e){
-			System.out.println("IOException is caught");
-			e.printStackTrace();
-			System.exit(-1);
-		}
-
-		//ArrayList<String> listOfUsers = new ArrayList<>();
-		//Reread the user list into the listOusers then add new user new to it
-		//obsList =  FXCollections.observableArrayList(listOfUsers);
-		//listView.setItems(obsList);
 	}
 
 	@FXML
 	public void deleteUser(ActionEvent evt) throws IOException {
-
+		//Get the item and index value for deletion of the particular song
+		String item = listView.getSelectionModel().getSelectedItem();
+	    int index = listView.getSelectionModel().getSelectedIndex();
+	    //Make sure an item is selected before being able to delete an entry
+	    if(item == null || index == -1) {    	
+	    	Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Error Dialog");
+			alert.setHeaderText("Must pick a user first");
+			alert.setContentText("A user must be picked in order to delete them from the database!");
+			alert.showAndWait();
+	    }else {	
+		    //Set up the path for which to search through
+			String dir = System.getProperty("user.dir");
+	        String path = dir+"/src/sample/users/";
+	        File folder = new File(path);
+			File[] files = folder.listFiles();
+			//Check if user directory exist, if not return back empty userNames
+			if(!folder.exists()) {
+				return;
+			}
+			//Search through the directory and all its subdirectory's recursively to find all users folders, and thier .ser files
+			for (File file : files) {
+				if (file.isDirectory()) {
+					//If directory, check to see if name matches, if so delete the directory
+					System.out.println(file.getName());
+					if(file.getName().equals(item)) {
+						deleteFolder(file);
+					}
+				} else {
+					//Else move on
+					continue;
+				}
+			}
+			updateListView();
+	    }
+	}
+	
+	private ArrayList<String> getUserNames(File currentDir){
+		ArrayList<String> userNames = new ArrayList<>();
+		String dir = System.getProperty("user.dir");
+        String path = dir+"/src/sample/users/";
+        File folder = new File(path);
+		//File[] listOfFiles = folder.listFiles();
+		File[] files = currentDir.listFiles();
+		
+        //Check if user directory exist, if not return back empty userNames
+		if(!folder.exists()) {
+			return userNames;
+		}
+		//Search through the directory and all its subdirectory's recursively to find all users folders, and their .ser files
+		for (File file : files) {
+			if (file.isDirectory()) {
+				//Add up all your current results found in this level
+				userNames.addAll(getUserNames(file));
+			} else {
+				//Check to see if file ends in .ser
+				if (file.length() > 4){
+				    String test = file.getName().substring(file.getName().length() - 4);
+				    //If so split string and take the left hand side and add it to the list
+				    if(test.equals(".ser")) {									  
+				    	String[] arrOfStr = file.getName().split("\\.", 2); 
+				    	 userNames.add(arrOfStr[0]);
+				    }
+				}
+			}
+		}		
+		return userNames;
+	}
+	
+	public static void deleteFolder(File folder) {
+	    File[] files = folder.listFiles();
+	    if(files!=null) { //some JVMs return null for empty dirs
+	        for(File f: files) {
+	            if(f.isDirectory()) {
+	                deleteFolder(f);
+	            } else {
+	                f.delete();
+	            }
+	        }
+	    }
+	    folder.delete();
+	}
+	
+	private void updateListView() {
+		String dir = System.getProperty("user.dir");
+        String path = dir+"/src/sample/users/";
+        File currentDir = new File(path);
+		//create an ObservableList from an ArrayList
+		ArrayList<String> listOfUsers = getUserNames(currentDir);
+		obsList =  FXCollections.observableArrayList(listOfUsers);		
+		listView.setItems(obsList);
 	}
 }
